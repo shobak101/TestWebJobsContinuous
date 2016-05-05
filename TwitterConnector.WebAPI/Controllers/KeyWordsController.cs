7241668b60d0398f037194c9887aebfb4919cbf4
Hosam Shobak <Hosams@microsoft.com>
2016-05-04 19:08:53 -0700

    adding API for the webjob

---
+++ TwitterConnector.WebAPI/Controllers/KeyWordsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+using System.Reflection;
+
+namespace TwitterConnector.WebAPI.Controllers
+{
+    public class KeyWordsController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            var ret = new KeyValuePair<string, string>("key", "value");
+            //File.ReadAllText(Path.Combine(Assembly.GetExecutingAssembly().Location,"KeyWords.txt"));
+            return Request.CreateResponse(HttpStatusCode.OK, ret);
+        }
+    }
+}
